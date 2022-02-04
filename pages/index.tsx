0643097c48f21f9caa9c6814ef49606faa88e6b7
tristan <69066026+twisttaan@users.noreply.github.com>
2022-02-04 23:14:23 +1100

    fix: discord invite

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,9 @@ const Home: NextPage = () => {
   return (
     <div className="bg-black">
       <div className="flex flex-col items-center justify-center h-screen">
-        <Link href="https://discord.gg/MKXkTHGq5J">
-          <a className="text-white hover:text-blue-400 text-5xl">soon ;)</a>
+        <Link href="https://discord.gg/7UwN2PZgva">
+          <span className="text-white hover:text-blue-400 text-3xl">EvieClient a 1.8 PvP Optimized Client</span>
+          <span className="text-white text-3xl">coming out <a className="hover:text-blue-400">soon!</a></span>
         </Link>
       </div>
     </div>
