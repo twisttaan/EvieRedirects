9fa2801fb25ced2508dc8d6d65d305a0c105958d
Tristan Camejo <twisttaan@icloud.com>
2022-02-07 16:29:57 +1100

    fix: hover effect feat: getLauncherJson

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,15 @@ const Home: NextPage = () => {
   return (
     <div className="bg-black">
       <div className="flex flex-col items-center justify-center h-screen">
-          <span className="text-white hover:text-blue-400 text-3xl">EvieClient a 1.8 PvP Optimized Client</span>
-          <span className="text-white text-3xl">coming out         <Link href="https://discord.gg/7UwN2PZgva"><a className="hover:text-blue-400">soon!</a></Link></span>
+        <span className="text-white text-3xl">
+          EvieClient a 1.8 PvP Optimized Client
+        </span>
+        <span className="text-white text-3xl">
+          coming out{" "}
+          <Link href="https://discord.gg/7UwN2PZgva">
+            <a className="hover:text-blue-400">soon!</a>
+          </Link>
+        </span>
       </div>
     </div>
   );
