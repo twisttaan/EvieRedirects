4670c9a68199c94c8d9a263a8ef42ca5ca744105
Tristan <69066026+twisttaan@users.noreply.github.com>
2022-02-09 14:34:33 +1100

    feat: /discord

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/VKaNS86wEN",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
