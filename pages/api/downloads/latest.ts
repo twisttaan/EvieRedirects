ab29031a503752db2ab7f54a5a3ed28966643b61
Tristan Camejo <twisttaan@icloud.com>
2022-02-14 17:46:20 +1100

    feat: latest download

---
+++ pages/api/downloads/latest.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  axios
+    .get(
+      "https://api.github.com/repos/EvieClient/EvieClient-Releases/releases/latest"
+    )
+    .then(function (response) {
+      const data = response.data;
+      const downloadURL = data.assets[0].browser_download_url;
+      res.status(200).redirect(downloadURL);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(200).json({
+        error: true,
+        message: "Looks like something went wrong here. :-)",
+      });
+    });
+}
