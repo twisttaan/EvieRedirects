2a5dde0950d1592fcb23cf985f552c6a936e5e6a
Tristan Camejo <twisttaan@icloud.com>
2022-02-14 17:54:10 +1100

    remove: rick roll

--- pages/api/downloads/v2/latest.ts
+++ pages/api/downloads/v2/latest.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).redirect("https://www.youtube.com/watch?v=-pHhb4biR9k");
+  res.status(200).send("falingdy is smart");
 }
