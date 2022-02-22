84eeba17901e120b972d2aee93b1e2c1a58f585e
Tristan Camejo <twisttaan@icloud.com>
2022-02-22 17:46:21 +1100

    add: more redirects

--- next.config.js
+++ next.config.js
@@ -3,10 +3,54 @@ const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
+      /*
+       * TeamEvie Link Shortcuts
+       */
+
       {
         source: "/discord",
         destination: "https://discord.gg/VKaNS86wEN",
         permanent: true,
+      },
+      {
+        source: "/bot",
+        destination: "https://eviebot.rocks",
+        permanent: false, // Migrating eviebot.rocks soon to this repo
+      },
+      {
+        source: "/team",
+        destination: "https://github.com/EvieClient",
+        permanent: false,
+      },
+      {
+        source: "/qwdl", // Quick Windows Download Link
+        destination: "https://evie.pw/api/downloads/latest?platform=win",
+        permanent: false,
+      },
+      {
+        source: "/qwml", // Quick Mac Download Link
+        destination: "https://evie.pw/api/downloads/latest?platform=mac",
+        permanent: false,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/EvieClient",
+        permanent: false,
+      },
+
+      /*
+       * Personal Link Shortcuts
+       */
+
+      {
+        source: "/tristan",
+        destination: "https://github.com/twisttaan",
+        permanent: false, // Once https://tristancamejo.com/ is finished, this will be permanent
+      },
+      {
+        source: "/tsmp",
+        destination: "https://tristansmp.com",
+        permanent: true, // Once https://tristancamejo.com/ is finished, this will be permanent
       },
     ];
   },
