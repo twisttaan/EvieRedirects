83b1577528712f02640679332f223727843afd58
Tristan Camejo <twisttaan@icloud.com>
2022-03-15 18:23:37 +1100

    fix: for later

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from "next";
 import Footer from "../components/Footer";
-import Space from "../components/info/Space";
 import Nav from "../components/nav";
 
 const About: NextPage = () => {
   return (
     <>
       <Nav />
-      <Space />
-
       <Footer />
     </>
   );
