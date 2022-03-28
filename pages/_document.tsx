ce25925acc2bc6ff723939d01de82a8cc8cb04bc
Tristan Camejo <twisttaan@icloud.com>
2022-03-28 17:29:47 +1100

    feat: evie tail

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="baa9a56a-a68f-4709-a600-cefcfa6f3fa2"
+          src="https://tail.evie.pw/umami.js"
+        />
+      </Head>
       <body className="bg-white text-black dark:bg-[#171a1c] dark:text-black">
         <Main />
         <NextScript />
